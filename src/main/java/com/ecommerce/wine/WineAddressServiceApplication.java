128ca61da3b594737f25ee431ef1826323672c15
Raúl Flores Rodríguez <raul@JARVIS.local>
2017-09-06 22:26:04 -0500

    Initial commit

---
+++ src/main/java/com/ecommerce/wine/WineAddressServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.wine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineAddressServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineAddressServiceApplication.class, args);
+	}
+}
